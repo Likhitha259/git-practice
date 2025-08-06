5bd2f06b8ec1a41cbebcf24254db70decb04d7a2
Likhitha <likhithaygramesh259@gmail.com>
2025-08-06 12:34:50 +0530

    new changes made in file m2

--- m2.py
+++ m2.py
@@ -1,6 +1,8 @@
 # print('this is file2 for practise')
 # '''this change is made in the github to know how the changes happen'''
-a=[1,2,9.8,{'a':7},'iu',[2,3,4]]
+
+# a=[1,2,9.8,{'a':7},'iu',[2,3,4]]
+a=eval(input())
 b='a'
 for i in a:
     if type(i)==dict:
