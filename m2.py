bce0a8767139a54fbe254f82f75fab9686384fe6
Likhitha <likhithaygramesh259@gmail.com>
2025-08-06 14:32:29 +0530

    stash changes observed

--- m2.py
+++ m2.py
@@ -1,5 +1,6 @@
 # print('this is file2 for practise')
 # '''this change is made in the github to know how the changes happen'''
+'''to know how stash work'''
 a=[1,2,9.8,{'a':7},'iu',[2,3,4]]
 b='a'
 for i in a:
