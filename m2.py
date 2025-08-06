4d49e3282ae89b63b2ffe7ac9b440c3cfe22a6a2
Likhitha <likhithaygramesh259@gmail.com>
2025-08-06 11:26:17 +0530

    new programs are added in m2 and m3 file

--- m2.py
+++ m2.py
@@ -1,2 +1,9 @@
-print('this is file2 for practise')
-'''this change is made in the github to know how the changes happen'''
+# print('this is file2 for practise')
+# '''this change is made in the github to know how the changes happen'''
+a=[1,2,9.8,{'a':7},'iu',[2,3,4]]
+b='a'
+for i in a:
+    if type(i)==dict:
+        if b in i.keys():
+           i[b]=34
+print(a)
