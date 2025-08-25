416aa0af5894eb914f592fb9327a6fe6e99b26b9
Likhitha259 <likhithaygramesh259@gmail.com>
2025-08-25 16:56:59 +0530

    Update m3.py

--- m3.py
+++ m3.py
@@ -1,1 +1,10 @@
-
+#this is program to get frequency of each word in the sentence
+def frequency_of_word(string):
+    out_dict={}
+    for word in string:
+        if out_dict.get(word,0)==0:
+            out_dict[word]=1
+        else:
+            out_dict[word]+=1
+    return out_dict
+print(frequency_of_word(input().split()))
