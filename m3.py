d3f1b7658ee77f33ea69266b3b203c7a8e1e9dfe
Likhitha259 <likhithaygramesh259@gmail.com>
2025-08-25 16:56:08 +0530

    Update m3.py

--- m3.py
+++ m3.py
@@ -1,9 +1,1 @@
-def frequency_of_word(string):
-    out_dict={}
-    for word in string:
-        if out_dict.get(word,0)==0:
-            out_dict[word]=1
-        else:
-            out_dict[word]+=1
-    return out_dict
-print(frequency_of_word(input().split()))        
+
